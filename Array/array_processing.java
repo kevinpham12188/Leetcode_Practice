4ba9021d7bd6dcfc61c63e5ebc259c0bc4f49cd4
Kevin Pham <kevinpham@Kevins-MBP.lan>
2022-08-13 14:29:57 -0400

    add array filtering

--- Array/array_processing.java
+++ Array/array_processing.java
@@ -1,7 +1,8 @@
 public class array_processing {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(number(new int[] {0, 1, 0, 1, 1, 0, 1, 1, 1}));
     }
+
     // Given an array and a number. Return the index of the frist found element in
     // the array. If not fount, return -1
     // [1,2,3,4], number = 4
@@ -14,7 +15,23 @@ public class array_processing {
             if (array[i] == number) {
                 return i;
             }
-        } return -1;
+        }
+        return -1;
+    }
+
+    // Given an array with 0 and 1. Count the max consecutive 1s in the array
+    // [0, 1, 0, 1, 1, 0, 1, 1, 1] => 3
+    public static int number(int[] array) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        } return maxCount;
     }
 
 }
