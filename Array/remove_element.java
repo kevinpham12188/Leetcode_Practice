50016a73eae80400a42ffe2531c91aac21dc9edb
kevinpham7788 <kevinpham7788@gmail.com>
2022-08-20 21:29:08 -0700

    Add remove element in array

---
+++ Array/remove_element.java
@@ -0,0 +1,20 @@
+public class remove_element {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    // nums = [3, 2, 2, 3], val = 3 => 2, nums = [2, 2, _, _]
+    public static int removeElement(int[] arr, int k) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == k) {
+                for (int j = i; j < n-1; j++) {
+                    arr[j] = arr[j+1];
+                }
+                i--;
+                n--;
+            }
+        }
+        return n;
+    }
+}
