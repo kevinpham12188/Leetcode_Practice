7b26dc92705f6c5c1d942f00eb4994d121bb7dfa
kevinpham7788 <kevinpham7788@gmail.com>
2022-11-07 20:55:02 -0500

    roman to integer

---
+++ String/_13_Roman_to_Integer.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _13_Roman_to_Integer {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> myMap = new HashMap<>();
+        myMap.put('I', 1);
+        myMap.put('V', 5);
+        myMap.put('X', 10);
+        myMap.put('L', 50);
+        myMap.put('C', 100);
+        myMap.put('D', 500);
+        myMap.put('M', 1000);
+
+        int sum = myMap.get(s.charAt(s.length()-1));
+        for (int i = s.length() - 2; i > 0; i--) {
+            if (myMap.get(s.charAt(i)) < sum) {
+                sum -= myMap.get(s.charAt(i));
+            } else {
+                sum += myMap.get(s.charAt(i));
+            }
+        }
+        return sum;
+    }
+}
